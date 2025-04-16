62e1309e0f11739bf15ca84b1b00ce7e7fab7472
Faysal Ahmed Fahim <ffahim2330717@bscse.uiu.ac.bd>
2025-04-16 17:04:40 +0600

    Add generic class implementation with type safety in TypeScript

---
+++ oop/generic_class.ts
@@ -0,0 +1,26 @@
+class Box<T> {
+    private value: T;
+    
+    constructor(value: T) {
+        this.value = value;
+    }
+    
+    getValue(): T {
+        return this.value;
+    }
+    
+    setValue(value: T): void {
+        this.value = value;
+    }
+}
+
+// Usage with different types
+const numberBox = new Box<number>(42);
+const stringBox = new Box<string>("Hello TypeScript");
+
+console.log(numberBox.getValue()); // 42
+console.log(stringBox.getValue()); // "Hello TypeScript"
+
+// Type safety is enforced
+numberBox.setValue(100);          // OK
+// numberBox.setValue("string");  // Error: Argument of type 'string' is not assignable to parameter of type 'number'
